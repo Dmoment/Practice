9502a5c59c9ec186ed27288eb64690a161f093b0
Deepak Chauhan <rishudc119@gmail.com>
2021-01-10 23:45:13 +0530

    Checking

--- practice.rb
+++ practice.rb
@@ -13,13 +13,14 @@ class UrlConfig
 end
 
 class ParsedTypeResponse
-  
+
   def initialize(url)
     @url = url
   end
 
   def fetched_type_attribute
     response = Net::HTTP.get_response config.base_url
+    puts response.body.class
     parsed_response = JSON.parse(response.body)
     type_attribute = []
     parsed_response.each do |data|
@@ -29,37 +30,28 @@ class ParsedTypeResponse
   end
 
   def config
-    @config ||= UrlConfig.new(@url)
+    @config = UrlConfig.new(@url)
   end
 end
 
 
+
 class TenderLoveScore
 
-  def initialize(url)
+  def initialize(url , event_type)
     @url = url
+    @event_type = event_type
   end
 
   def score
    types = commit_type.fetched_type_attribute
    sum = 0
    types.each do |type|
-    case type
-    when "IssuesEvent"
-      sum += 7
-    when "IssueCommentEvent"
-      sum += 6
-    when "PushEvent"
-      sum += 5
-    when "PullRequestReviewCommentEvent"
-      sum += 4
-    when "WatchEvent"
-      sum += 3
-    when "CreateEvent"
-      sum += 2
+    if @event_type.key?(type)
+      sum += @event_type[type]
     else
-      sum += 1
-    end  
+      sum += 1  
+    end
    end
    return "TenderLove's github score is #{sum}"
   end
@@ -69,7 +61,13 @@ class TenderLoveScore
   end
 end
 
-
+event_type = {"IssuesEvent" => 7,
+              "IssueCommentEvent" => 6,
+              "PushEvent" => 5,
+              "PullRequestReviewCommentEvent" => 4,
+              "WatchEvent" => 3,
+              "CreateEvent" => 2
+              }
 tender_url = "https://api.github.com/users/tenderlove/events/public"
-tende_love_score = TenderLoveScore.new(tender_url)
+tender_love_score = TenderLoveScore.new(tender_url , event_type)
 puts tender_love_score.score
